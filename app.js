74259501461f0e67357b376587b62564ae6c28e3
wambuguedison <wambuguedison@gmail.com>
2020-10-01 16:38:19 +0300

    Intergrate body-parser

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const cors = require('cors');
+const bodyParser = require('body-parser');
 
 const app = express();
 app.use(cors());
+app.use(bodyParser.json());
+app.use(
+  bodyParser.urlencoded({
+    extended: true
+  })
+);
 
 app.get('/', (req, res, next) => {
   res.status(200).json({
