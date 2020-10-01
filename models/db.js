84461ab03ddcb78f55159e87fd7d954893d496ab
wambuguedison <wambuguedison@gmail.com>
2020-10-01 18:54:09 +0300

    Register Admin db

--- models/db.js
+++ models/db.js
@@ -3,3 +3,5 @@ const nedb = require('nedb');
 exports.events = new nedb({ filename: './models/events.db', autoload: true });
 
 exports.prayers = new nedb({ filename: './models/prayers.db', autoload: true });
+
+exports.admins = new nedb({ filename: './models/admins.db', autoload: true });
