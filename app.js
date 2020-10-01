2bcb7bde5f92e19e53a35659fa36c2c72d0642a8
wambuguedison <wambuguedison@gmail.com>
2020-10-01 16:42:40 +0300

    Add prayer and event post route

--- app.js
+++ app.js
@@ -23,9 +23,23 @@ app.get('/prayers', (req, res, next) => {
   });
 });
 
+app.post('/prayer', (req, res, next) => {
+  console.log(req.body);
+  res.status(201).json({
+    message: 'prayer added successfully!'
+  });
+});
+
 app.get('/events', (req, res, next) => {
   res.status(200).json({
     events: []
+  });
+});
+
+app.post('/event', (req, res, next) => {
+  console.log(req.body);
+  res.status(201).json({
+    message: 'Event created successfully!'
   });
 });
 
