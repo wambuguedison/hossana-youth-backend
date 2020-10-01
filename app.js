4f3aba9cfa7084d24d8a58a3e91744e4484274b5
wambuguedison <wambuguedison@gmail.com>
2020-10-01 18:34:17 +0300

    Optimize code structure

--- app.js
+++ app.js
@@ -13,72 +13,13 @@ app.use(
 );
 
 const events = require('./routes/events');
+const prayers = require('./routes/prayers');
 app.use('', events);
+app.use('', prayers);
 
 app.get('/', (req, res, next) => {
   res.status(200).json({
     message: 'we are the youth'
-  });
-});
-
-app.get('/prayers', (req, res, next) => {
-  prayers.find({}, (err, prayers) => {
-    if (err) {
-      res.status(500).json({
-        error: err
-      });
-    }
-
-    res.status(200).json({
-      prayers
-    });
-  });
-});
-
-app.post('/prayer', (req, res, next) => {
-  const prayer = {
-    title: req.body.title,
-    deleted: 0,
-    user: req.id,
-    created_at: new Date().toDateString()
-  };
-  prayers.insert(prayer, (err, prayer) => {
-    if (err) {
-      res.status(500).json({
-        error: err
-      });
-    }
-    let added_prayer = {
-      title: prayer.title,
-      id: prayer._id
-    };
-    res.status(201).json(added_prayer);
-  });
-});
-
-app.put('/prayer/:id', (req, res, next) => {
-  const new_prayer = {
-    title: req.body.title,
-    deleted: 0,
-    created_at: new Date().toDateString()
-  };
-  prayers.update({ _id: req.params.id }, new_prayer, {}, (err, numReplaced) => {
-    if (err) {
-      res.status(500).json({
-        error: err
-      });
-    }
-    let edited_prayer = {
-      title: new_prayer.title,
-      id: new_prayer._id
-    };
-    res.status(201).json(edited_prayer);
-  });
-});
-
-app.delete('/prayer/:id', (req, res, next) => {
-  prayers.remove({ _id: req.params.id }, {}, (err, numRemoved) => {
-    res.status(204).send('Prayer deleted successfully');
   });
 });
 
